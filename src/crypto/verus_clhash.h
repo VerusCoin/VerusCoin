f7d74dbe37e50e589ff01431572e477c485fe8e1
Asher Dawes <asher.dawes@gmail.com>
2019-01-02 17:17:33 -0800

    debugging thread_specific pointer

--- src/crypto/verus_clhash.h
+++ src/crypto/verus_clhash.h
@@ -80,6 +80,8 @@ struct thread_specific_ptr {
 #else
     ~thread_specific_ptr() {
         printf("~thread_specific_ptr()\n");
+        printf(%p, this->get())
+        printf(%p ,this->ptr)
         this->reset();
         printf("after reset\n");
 
